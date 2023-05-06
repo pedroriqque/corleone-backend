e33591415fae6e3548831c37279bf30593d05e55
pedroriqque <dev.pedro@outlook.com>
2023-05-05 22:28:23 -0300

    environment variables

--- src/main.ts
+++ src/main.ts
@@ -13,6 +13,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, swaggerConfig);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(3000);
+  await app.listen(process.env.API_PORT || 3333);
 }
+
 bootstrap();
